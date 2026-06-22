6f41892feb8db309d24ece43677893ecaec4ca13
Billy Wu <biyi.wu@gmail.com>
2026-06-22 12:58:05 +1000

    Creating a portfolio new website from scratch

---
+++ billywu_site/src/constants/index.js
@@ -0,0 +1,10 @@
+export const words = [
+    { text: 'Ideas', imgPath: '/images/ideas.svg' },
+    { text: 'Concepts', imgPath: '/images/ideas.svg' },
+    { text: 'Designs', imgPath: '/images/ideas.svg' },
+    { text: 'Code', imgPath: '/images/ideas.svg' },
+    { text: 'Ideas', imgPath: '/images/ideas.svg' },
+    { text: 'Concepts', imgPath: '/images/ideas.svg' },
+    { text: 'Designs', imgPath: '/images/ideas.svg' },
+    { text: 'Code', imgPath: '/images/ideas.svg' },
+]
