07ed61fcc79b1b4f949b458be7f9e72f4ca2a8cc
Billy Wu <biyi.wu@gmail.com>
2026-06-24 11:55:06 +1000

    Added Contact section template

--- billywu_site/src/App.jsx
+++ billywu_site/src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar.jsx";
 import FeatureCards from "./sections/FeatureCards.jsx";
 import ExperienceSection from "./sections/ExperienceSection.jsx";
 import TechStack from "./sections/TechStack.jsx";
+import Contact from "./sections/Contact.jsx";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
             <FeatureCards />
             <ExperienceSection />
             <TechStack />
+            <Contact />
         </>
     )
 }
