0fef7f1bfa89fc0ca16a01d6c5172ddbb87a1302
Billy Wu <biyi.wu@gmail.com>
2026-06-24 12:05:20 +1000

    Added Footer

--- billywu_site/src/App.jsx
+++ billywu_site/src/App.jsx
@@ -5,6 +5,7 @@ import FeatureCards from "./sections/FeatureCards.jsx";
 import ExperienceSection from "./sections/ExperienceSection.jsx";
 import TechStack from "./sections/TechStack.jsx";
 import Contact from "./sections/Contact.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
     return (
@@ -16,6 +17,7 @@ const App = () => {
             <ExperienceSection />
             <TechStack />
             <Contact />
+            <Footer />
         </>
     )
 }
