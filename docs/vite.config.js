3b9a0f903d8e36f0e8273aee3058955426d6b428
Billy Wu <biyi.wu@gmail.com>
2026-06-26 22:40:19 +1000

    Test

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
+  build: {
+    outDir: '../',
+    emptyOutDir: false,
+  },
   plugins: [
       react(),
       tailwindcss()
